69df78342f4b0f25d7f5659712d42fcc863ea690
ydanielsb97 <ydanielsb97@gmail.com>
2021-06-13 19:31:16 -0400

    Creacion de contexto

---
+++ Shop.Web/Data/DataContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Shop.Web.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Shop.Web.Data.Entities;
+
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -4,10 +4,13 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Shop.Web.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace Shop.Web
 {
@@ -23,12 +26,28 @@ namespace Shop.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<CookiePolicyOptions>(options =>
+
+            {
+
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+
+                options.CheckConsentNeeded = context => true;
+
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+
+            });
+
+
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+  
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
